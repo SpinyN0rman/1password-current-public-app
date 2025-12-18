3abf93f857d236e78e4294c53f21c995534d589e
Andi Northrop <andi.northrop@agilebits.com>
2025-12-18 15:24:23 +0000

    Refactors code, expands db initialisation, implements api call for edge

--- functions.py
+++ functions.py
@@ -1,1 +1,127 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, timezone
+import streamlit as st
+from sqlalchemy import text
+from pandas.core.methods.to_dict import to_dict
 
+@st.cache_data(ttl=60)
+def init_db():
+    conn = st.connection('versions_db', type='sql')
+    with conn.session as s:
+        s.execute(text(
+            'CREATE TABLE IF NOT EXISTS versions (id TEXT, browser TEXT, channel TEXT, version TEXT, success_check TEXT, fail_check TEXT, error_message TEXT);'))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+            SELECT 'chrome_stable', 'chrome', 'stable', '', '0', '0', ''
+            WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'chrome_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'edge_stable', 'edge', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'edge_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'firefox_stable', 'firefox', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'firefox_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'safari_stable', 'safari', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'safari_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'opi_stable', 'opi', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'opi_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'opa_stable', 'opa', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'opa_stable');"""
+        ))
+        s.execute(text(
+            """INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message)
+               SELECT 'opw_stable', 'opw', 'stable', '', '0', '0', ''
+               WHERE NOT EXISTS (SELECT 1 FROM versions WHERE id = 'opw_stable');"""
+        ))
+        s.commit()
+
+# Set up our database connection
+@st.cache_data(ttl=60)
+def db_dict():
+    conn = st.connection('versions_db', type='sql', ttl=60)
+
+    # Query the database, which returns a pandas dataframe, then convert it to a dict
+    versions = conn.query('select * from versions')
+    versions_dict = to_dict(versions, orient='index')
+    return versions_dict
+
+
+@st.cache_data(ttl=60)
+def should_we_scrape():
+    conn = st.connection('versions_db', type='sql')
+    versions = conn.query('select * from versions')
+    versions_dict = to_dict(versions, orient='index')
+
+    next_check = datetime.now() + timedelta(hours=1)
+    next_check_epoch = next_check.timestamp()
+
+    for index, version in enumerate(versions_dict):
+        if float(versions_dict[index]['success_check']) > next_check_epoch or float(versions_dict[index]['fail_check']) > next_check_epoch:
+            return True
+
+    return False
+
+def format_datetime(date_time):
+    utc_time = datetime.fromtimestamp(float(date_time), timezone.utc)
+    local_time = utc_time.astimezone()
+    formatted_time = local_time.strftime('%Y-%m-%d %H:%M')
+    return formatted_time
+
+def edge_stable_api():
+    url = 'https://microsoftedge.microsoft.com/addons/getproductdetailsbycrxid/dppgmdbiimibapkepcbdbmkaabgiofem?hl=en-US'
+    try:
+        response = requests.get(url)
+        response_json = response.json()
+        version = response_json['version']
+        success_time = datetime.now().timestamp()
+        fail_time = ""
+        error_msg = ""
+    except Exception as e:
+        version = ""
+        success_time = ""
+        fail_time = datetime.now().timestamp()
+        error_msg = e
+    conn = st.connection('versions_db', type='sql')
+    if success_time:
+        with conn.session as s:
+            s.execute(text(
+                "UPDATE versions SET version = :edge_version, success_check = :edge_success WHERE id = :edge_stable;"),
+                {"edge_version": version, "edge_success": success_time, "edge_stable": "edge_stable"},)
+            s.commit()
+    elif fail_time:
+        with conn.session as s:
+            s.execute(text(
+                "UPDATE versions SET fail_check = :edge_fail, error_message = :edge_error WHERE id = :edge_stable;"),
+                {"edge_fail": fail_time, "edge_error": error_msg, "edge_stable": "edge_stable"},)
+            s.commit()
+    return
+
+def chrome_stable_scrape():
+    url = 'https://chromewebstore.google.com/detail/1password-%E2%80%93-password-mana/aeblfdkhhhdcdjpifhhbdiojplfjncoa'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return
+
+
+# chrome beta: https://chromewebstore.google.com/detail/1password-beta-%E2%80%93-password/khgocmkkpikpnmmkgmdnfckapcdkgfaf
+# chrome nightly: https://chromewebstore.google.com/detail/1password-nightly-%E2%80%93-passw/gejiddohjgogedgjnonbofjigllpkmbf
+# firefox stable: https://addons.mozilla.org/en-US/firefox/addon/1password-x-password-manager/
+# firefox beta: https://c.1password.com/dist/1P/b5x/firefox/beta/updates.json
+# firefox nightly: https://c.1password.com/dist/1P/b5x/firefox/nightly/updates.json
+# safari: https://itunes.apple.com/lookup?bundleId=%s com.1password.safari
+# opi: https://itunes.apple.com/lookup?bundleId=%s com.1password.1password
+# opa:
+# opw:
--- web.py
+++ web.py
@@ -1,32 +1,25 @@
 import streamlit as st
 from pandas.core.methods.to_dict import to_dict
-from sqlalchemy import text
+import functions as func
 
-conn = st.connection('versions_db', type='sql')
+st.set_page_config(layout="wide")
 
-with conn.session as s:
-    s.execute(text('CREATE TABLE IF NOT EXISTS versions (id TEXT, browser TEXT, channel TEXT, version TEXT, success_check TEXT, fail_check TEXT, error_message TEXT);'))
-    s.execute(text('DELETE FROM versions;'))
-    browsers = {'chrome_stable': ['chrome', 'stable', '8.11.22', '2025-12-17:10:18', '', ''], 'edge_stable': ['edge', 'stable', '8.11.22', '2025-12-17:10:18', '', ''], 'firefox_stable': ['firefox', 'stable', '8.11.16', '2025-11-10:06:10', '2025-12-17:10:18', 'scrape failed']}
-    for k in browsers:
-        s.execute(text(
-            'INSERT INTO versions (id, browser, channel, version, success_check, fail_check, error_message) VALUES (:id, :browser, :channel, :version, :success_check, :fail_check, :error_message);'),
-            params=dict(id=k, browser=browsers[k][0], channel=browsers[k][1], version=browsers[k][2], success_check=browsers[k][3], fail_check=browsers[k][4], error_message=browsers[k][5])
-        )
-    s.commit()
+# Initialise the database, if it doesn't already exist
+func.init_db()
 
-# Query and display the data you inserted
-versions = conn.query('select * from versions')
-st.dataframe(versions)
-versions_dict = to_dict(versions, orient='index')
+# Scrape if the last check was more than an hour ago
+perform_scrape = func.should_we_scrape()
+if perform_scrape:
+    func.edge_stable_api()
+
+# Fetches a dictionary from the database
+versions_dict = func.db_dict()
 
 cols = st.columns(len(versions_dict), border=True)
 
 for index, col in enumerate(cols):
     with col:
-        # for version, index in enumerate(versions_dict):
-        # flex = st.container(border=True, horizontal=True, horizontal_alignment="right")
-        if versions_dict[index]['fail_check'] == "":
+        if versions_dict[index]['success_check'] > versions_dict[index]['fail_check']:
             fail_indicator = "🟢"
         else:
             fail_indicator = "🔴"
@@ -37,7 +30,7 @@ for index, col in enumerate(cols):
             st.write(f"{fail_indicator}")
         st.caption(f"*{versions_dict[index]['channel']}*")
         st.write(f"**Version: `{versions_dict[index]['version']}`**")
-        st.write(f"*Last successful check: **{versions_dict[index]['success_check']}***")
-        if versions_dict[index]['fail_check'] != "":
+        st.write(f"*Last successful check: **{func.format_datetime(versions_dict[index]['success_check'])}***")
+        if versions_dict[index]['fail_check'] > versions_dict[index]['success_check']:
             st.write(f"*Last failed check: **{versions_dict[index]['fail_check']}** "
                      f"with error message: **{versions_dict[index]['error_message']}***")
